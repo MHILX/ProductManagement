d40d5ff09c4b43b0b9c74070d62f3b6c990fbf15
Mohammed Hoque <mohammed.hoque@intelex.com>
2025-02-05 13:57:16 -0600

    Update CI workflow to use solution file and enhance project settings; add integration test for API

---
+++ src/ProductManagement.Test/ApiIntegrationTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Xunit;
+
+namespace ProductManagement.Test
+{
+    public class ApiIntegrationTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        // private readonly HttpClient _client;
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public ApiIntegrationTest(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            //_client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            //{
+            //    AllowAutoRedirect = false
+            //});
+        }
+
+        [Fact(Skip = "Skipping this test for now")]
+        public async Task Get_Products_ReturnsSuccessStatusCode()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            var request = "/api/products";
+
+            // Act
+            var response = await client.GetAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            var responseString = await response.Content.ReadAsStringAsync();
+            Assert.NotEmpty(responseString);
+        }
+    }
+}
